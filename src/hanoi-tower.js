ed0f9e0f8a0e6a4c648d8ffc32aa454400a99372
nikitar69 <nikitar69@gmail.com>
2021-03-30 00:17:35 +0200

    complete 6 of 10 tasks

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -4,3 +4,10 @@ module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let movesTowin = {};
+  movesTowin.turns = 2 ** disksNumber - 1;
+  movesTowin.seconds = Math.floor((movesTowin.turns * 3600) / turnsSpeed);
+  return movesTowin;
+};
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    let current;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        current = this.calculateDepth(arr[i]) + 1;
+      }
+      if (depth < current) {
+        depth = current;
+      }
+    }
+    return depth;
   }
 };
